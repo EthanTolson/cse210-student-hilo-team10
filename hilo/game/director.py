765ada25a21a0c01e21fa5cd56d245e60179edf4
Ethan Tolson <tol18009@byui.edu>
2021-10-04 12:30:55 -0700

    Created Bare Bones Layout For HiLo Game, Edited Main, Director, Shuffler Files

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,3 +1,19 @@
 """Director Class"""
+from game.shuffler import Shuffler
+
 class Director:
-    pass
+
+    def __init__(self):
+        pass
+    
+    def startGame(self):
+        pass
+
+    def getInputs(self):
+        pass
+
+    def doUpdates(self):
+        pass
+
+    def doOutputs(self):
+        pass
