ee79845f5e23ac861a491e9fa895d1aa4f4c245a
Ethan Tolson <tol18009@byui.edu>
2021-10-07 11:56:20 -0700

    Added comment section to each function and to both classes

--- hilo/game/shuffler.py
+++ hilo/game/shuffler.py
@@ -2,15 +2,20 @@
 import random as r
 
 class Shuffler:
+    """Attributes:"""
     def __init__(self):
+        """Args:"""
         pass
 
     def canPlay(self):
+        """Args:"""
         pass
 
     def getScore(self):
+        """Args:"""
         pass
 
     def getCard(self):
+        """Args:"""
         pass
     
