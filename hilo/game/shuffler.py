5676f8a8f4fe23e5cd098d9be56752ac7f75d515
austin eldredge <eld20009@byui.edu>
2021-10-07 13:42:10 -0600

    worrked on getScore function

--- hilo/game/shuffler.py
+++ hilo/game/shuffler.py
@@ -12,8 +12,15 @@ class Shuffler:
         pass
 
     def getScore(self):
-        """Args:"""
-        pass
+        """Args:
+        self: an instance of shuffler"""
+        #True if the users card is greater than the shufflers
+        #False if its less than the shufflers
+        if self.card[0] > self.card[1]:
+            return True
+        if self.card[1] >= self.card[0]:
+            return False
+        
 
     def getCard(self):
         """Args:"""
