ae9999218fb3ca4bee60e655b1d7431085e6c1f1
miskindr <91636488+miskindr@users.noreply.github.com>
2021-10-07 13:44:01 -0600

    init and startgame

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -6,11 +6,20 @@ class Director:
 
     def __init__(self):
         """Args:"""
+        self.keep_playing = True
+        self.score_points = True
+
         pass
     
     def startGame(self):
-        """Args:"""
-        pass
+        """Makes a loop to make sure that the game keeps playing as long as there are 
+            enough points and the player still wants to play
+            Args: self """
+        while self.keep_playing:
+            self.getInputs()
+            self.doUpdates()
+            self.doOutputs()
+
 
     def getInputs(self):
         """Args:"""
